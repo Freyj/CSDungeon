38cd3036169dd7c1964f54de56bb690a24be6d35
E1drad <charles.eric44@gmail.com>
2017-02-17 11:44:37 +0100

    fix to fit to the code convention

--- src/client.c
+++ src/client.c
@@ -3,35 +3,35 @@
  * @Date : 2017
  * @Version : 0.1
  * @brief : 
- */
+*/
 
 /* Structure pour les armes */
 typedef struct arme {
-  char* nom;
-  bool estMagique;
-  int degats;
-  int precision;
+	char* nom;
+	bool estMagique;
+	int degats;
+	int precision;
 }arme;
 
 /* Structure pour stocker les infos du client */
 typedef struct infoclient {
-  /* nom  */
-  char* nom;
-  /* caractéristiques */
-  int pv;
-  int pvMax;
-  int exp;
-  int niveau;
-  int force;
-  int magie;
-  int technique;
-  int vitesse;
-  int chance;
-  int defense;
-  int resistance;
-  arme arme;
-  /*socket associé */
-  
+	/* nom  */
+	char* nom;
+	/* caractéristiques */
+	int pv;
+	int pvMax;
+	int exp;
+	int niveau;
+	int force;
+	int magie;
+	int technique;
+	int vitesse;
+	int chance;
+	int defense;
+	int resistance;
+	arme arme;
+	/*socket associé */
+
 }infoclient;
 
 
--- src/thread_server.c
+++ src/thread_server.c
@@ -24,31 +24,31 @@ pthread_create(&thr, NULL, &hello, NULL);
 */
 /* Structure pour les armes */
 typedef struct arme {
-  char* nom;
-  bool estMagique;
-  int degats;
-  int precision;
+	char* nom;
+	bool estMagique;
+	int degats;
+	int precision;
 }arme;
 
 /* Structure pour stocker les infos du client */
 typedef struct infoclient {
-  /* nom  */
-  char* nom;
-  /* caractéristiques */
-  int pv;
-  int pvMax;
-  int exp;
-  int niveau;
-  int force;
-  int magie;
-  int technique;
-  int vitesse;
-  int chance;
-  int defense;
-  int resistance;
-  arme arme;
-  /*socket associé */
-  
+	/* nom  */
+	char* nom;
+	/* caractéristiques */
+	int pv;
+	int pvMax;
+	int exp;
+	int niveau;
+	int force;
+	int magie;
+	int technique;
+	int vitesse;
+	int chance;
+	int defense;
+	int resistance;
+	arme arme;
+	/*socket associé */
+
 }infoclient;
 
 /* Thread pour faire des trucs */
@@ -57,29 +57,28 @@ infoclient* infoTest;
 
 /* Fonction qui fait tourner le tour*/
 void* tourDeJeu(){
-  printf("%10s", "tour");
-  infoTest = malloc(sizeof(infoclient));
-  infoTest->nom = "Bouh";
-  if (infoTest->nom = "Bouh") {
-    printf("%10s", "test");
-  }
+	printf("%10s", "tour");
+	infoTest = malloc(sizeof(infoclient));
+	infoTest->nom = "Bouh";
+	if (infoTest->nom = "Bouh") {
+		printf("%10s", "test");
+	}
 
 }
 
 
 int main(void) {
 
-  ptr_thread = malloc(sizeof(pthread_t));
-  
-  int mainProc = pthread_create(ptr_thread, NULL, &tourDeJeu, NULL);
-  
-  if (mainProc == 0 ) {
-    printf("%10s","Thread de jeu terminé\0");
-    
-  }
-  else {
-    printf("%10s", mainProc);
-    printf("%10s","Lancement du thread de jeu échoué\0");
-  }
-  return 0;
+	ptr_thread = malloc(sizeof(pthread_t));
+
+	int mainProc = pthread_create(ptr_thread, NULL, &tourDeJeu, NULL);
+
+	if (mainProc == 0 ) {
+		printf("%10s","Thread de jeu terminé\0");
+
+	} else {
+		printf("%10s", mainProc);
+		printf("%10s","Lancement du thread de jeu échoué\0");
+	}
+	return 0;
 }
