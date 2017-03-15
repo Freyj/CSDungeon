2d530bb641c9b00e925f798c114d9ffb4e20b8f7
E1drad <charles.eric44@gmail.com>
2017-03-15 12:08:17 +0100

    déplacement de fichier et suppression d'ancien fichier

--- src/client/client.c
+++ src/client/client.c
@@ -7,37 +7,66 @@
 
 /*-----------------------------------------------------------
   Client a lancer apres le serveur avec la commande :
-  client <adresse-serveur> <pseudo>
-  ensuite les commandes sont du type : 
-  [attaquer|soigner] cible [option]
-  le serveur gèrera l'ordre
+  client <adresse-serveur> <message-a-transmettre>
   ------------------------------------------------------------*/
-#include "client.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-	int  socket_descriptor; 	/* descripteur de socket */
-	int  longueur; 				/* longueur d'un buffer utilisé */
+typedef struct sockaddr 
+sockaddr;
+
+typedef struct sockaddr_in 
+sockaddr_in;
+
+typedef struct hostent 
+hostent;
+
+typedef struct servent 
+servent;
+
+/* Structure pour les armes */
+typedef struct arme {
+	char* nom;
+	int estMagique;
+	int degats;
+	int precision;
+}arme;
+
+/* Structure pour stocker les infos du client */
+typedef struct infoclient {
+	/* nom  */
+	char* nom;
+	/* caractéristiques */
+	int pv;
+	int pvMax;
+	int exp;
+	int niveau;
+	int force;
+	int magie;
+	int technique;
+	int vitesse;
+	int chance;
+	int defense;
+	int resistance;
+	arme arme;
+	/*socket associé */
+}infoclient;
+
+
+void sendMessage(int port, char* host, char* mesg) {
+	int socket_descriptor; 		/* descripteur de socket */
+	int longueur; 				/* longueur d'un buffer utilisé */
 
 	sockaddr_in adresse_locale; /* adresse de socket local */
 
-	hostent * ptr_host;  		/* info sur une machine hote */
-	servent * ptr_service;  	/* info sur service */
+	hostent* ptr_host;  		/* info sur une machine hote */
+	servent* ptr_service;  		/* info sur service */
+
 	char buffer[256];
-	char * prog; 				/* nom du programme */
-	char * host; 				/* nom de la machine distante */
-	char * mesg; 				/* message envoyé */
-	int port; /*le port de connexion*/
-	if (argc != 4) {
-		perror("usage : client <adresse-serveur> <port> <message-a-transmettre>");
-		exit(1);
-	}
-	prog = argv[0];
-	host = argv[1];
-	port = atoi(argv[2]);
-	mesg = argv[3];
-	printf("nom de l'executable : %s \n", prog);
-	printf("adresse du serveur  : %s \n", host);
-	printf("message envoye      : %s \n", mesg);
 	if ((ptr_host = gethostbyname(host)) == NULL) {
 		perror("erreur : impossible de trouver le serveur a partir de son adresse.");
 		exit(1);
@@ -46,7 +75,8 @@ int main(int argc, char **argv) {
 	bcopy((char*)ptr_host->h_addr, (char*)&adresse_locale.sin_addr, ptr_host->h_length);
 	adresse_locale.sin_family = AF_INET; /* ou ptr_host->h_addrtype; */
 
-	adresse_locale.sin_port = htons(port);
+	//adresse_locale.sin_port = htons(7332); // why port 7332 ?
+	adresse_locale.sin_port = htons(5000);
 
 	printf("numero de port pour la connexion au serveur : %d \n", ntohs(adresse_locale.sin_port));
 
@@ -61,23 +91,65 @@ int main(int argc, char **argv) {
 		perror("erreur : impossible de se connecter au serveur.");
 		exit(1);
 	}
-
-	printf("connexion etablie avec le serveur. \n");
-	printf("envoi d'un message au serveur. \n");
-
-	/* envoi du message vers le serveur */
 	if ((write(socket_descriptor, mesg, strlen(mesg))) < 0) {
 		perror("erreur : impossible d'ecrire le message destine au serveur.");
 		exit(1);
 	}
-
 	printf("message envoye au serveur. \n");
-	/* lecture de la reponse en provenance du serveur */
+	//write(1,buffer,longueur);
+	//return (longueur = read(socket_descriptor, buffer, sizeof(buffer)));
 	while((longueur = read(socket_descriptor, buffer, sizeof(buffer))) > 0) {
 		printf("reponse du serveur : \n");
 		write(1,buffer,longueur);
-	}
+    }
+    printf("\n");
+    close(socket_descriptor);
 
+}
+
+
+int main(int argc, char **argv) {
+	int socket_descriptor; 		/* descripteur de socket */
+//	int longueur; 				/* longueur d'un buffer utilisé */
+
+//	sockaddr_in adresse_locale; /* adresse de socket local */
+
+//	hostent* ptr_host;  		/* info sur une machine hote */
+//	servent* ptr_service;  		/* info sur service */
+//	char buffer[256];
+	char* prog; 				/* nom du programme */
+	char* host; 				/* nom de la machine distante */
+	char* mesg = malloc(256); 	/* message envoyé */
+	char* nomClient;
+	int numeroPort;
+	if (argc != 4) {
+		perror("usage : client <adresse-serveur> <numero-port> <client-name>");
+		exit(1);
+	}
+	
+	prog = argv[0];
+	host = argv[1];
+	numeroPort = atoi(argv[2]);
+	nomClient = argv[3];
+
+	printf("nom de l'executable : %s \n", prog);
+	printf("adresse du serveur  : %s \n", host);
+	sendMessage(numeroPort, host, nomClient);
+	int endConnection;
+	endConnection = 1;
+	while(endConnection > 0) {
+		printf("Message : ");
+		scanf("%s", mesg);
+		printf("%s \n", mesg);
+
+		sendMessage(numeroPort, host, mesg);
+			if (strcmp("quit",mesg) == 0) {
+				endConnection = 0;
+			} else {
+				endConnection = 1;
+			}
+	}
+	free(mesg);
 	printf("\nfin de la reception.\n");
 	close(socket_descriptor);
 	printf("connexion avec le serveur fermee, fin du programme.\n");
